2eddddc75dbe718b61730f6f02029adefca0382f
Bharat <bharat@bitsoft.com>
2024-07-15 10:00:57 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// src/App.js
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:8000/api/items/')
+      .then(response => {
+        setItems(response.data);
+      })
+      .catch(error => {
+        console.error('There was an error fetching the data!', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Items</h1>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
